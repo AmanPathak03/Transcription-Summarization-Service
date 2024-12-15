3a9ddb11fb5248f7b82a3de925c7e185a8888728
Aman Pathak <pathakamaniaf2003@gmail.com>
2024-12-16 00:05:36 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist', // Ensure output directory matches Vercel's setting
+    sourcemap: true, // Optional: Helps with debugging on Vercel
+  },
+  server: {
+    port: 5173, // Ensure Vite uses port 3000 for local development
+  },
 });
